01ffaec703b619ebd7fcdb91804c0558abe8888a
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-04-11 23:48:19 +0500

    update lil

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const passport = require("passport");
 
 module.exports = (app) => {
-  // app.get("/", (req, res) => {
-  //   res.send("<h1>yahoo</h1>");
-  // });
+  app.get("/", (req, res) => {
+    res.send("<h1>yahoo</h1>");
+  });
 
   app.get(
     "/auth/google",
