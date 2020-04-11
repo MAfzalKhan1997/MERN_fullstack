d3ebbbdfc72decb3dcfc25df750b435705f0e639
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-04-12 00:07:08 +0500

    remove code to check

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const keys = require("./config/keys");
+// const mongoose = require("mongoose");
+// const cookieSession = require("cookie-session");
+// const passport = require("passport");
+// const keys = require("./config/keys");
 
-require("./models/User");
-require("./services/passport");
+// require("./models/User");
+// require("./services/passport");
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+// mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 100,
-    keys: [keys.cookieKey],
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 100,
+//     keys: [keys.cookieKey],
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
