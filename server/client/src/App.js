2913fff24f7e45928fa59b1cd0b22e4ead4bd661
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-04-15 23:53:48 +0500

    fix login issue

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,19 +5,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="/auth/google"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          Sign in with Google
-        </a>
+        <a href="/auth/google">Sign in with Google</a>
       </header>
     </div>
   );
