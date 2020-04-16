2a2b3d0b5750e353d09b5fe580ecb4f4bedfc43d
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-04-17 02:10:10 +0500

    setting react with redux

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+// import "typeface-roboto";
 
 function App() {
   return (
-    <div className="App">
+    <center>
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a href="/auth/google">Sign in with Google</a>
       </header>
-    </div>
+    </center>
   );
 }
 
