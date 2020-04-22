1bb2c2ae48b213bc35da6831c83821d227c15a46
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-04-23 03:22:44 +0500

    adding sendgrid configurations

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,24 @@
+const mongoose = require("mongoose");
 const requireAuth = require("../middlewares/requireAuth");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+
+const Survey = require("../models/Survey");
 
 module.exports = (app) => {
-  app.post("/api/survey", requireAuth, requireCredits, async (req, res) => {});
+  app.post("/api/survey", requireAuth, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map((email) => ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+  });
 };
